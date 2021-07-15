522d85f3dbce80bc11edf4107d6a6d171a47a1b0
lukbarrosBR <lbtecnologia@gmail.com>
2021-07-15 19:55:43 -0300

    Criando o controler e view base para users + adcionando layout.app

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Listing users
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('user.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [UserController::class, 'index']);
